054b7641c2986834c512291922aa1ecc20b3cf59
jgontrum <gontrum@me.com>
2018-03-15 18:27:36 +0100

    Fix grammar transform

--- ctf_parser/grammar/transform.py
+++ ctf_parser/grammar/transform.py
@@ -1,14 +1,28 @@
+import json
 import math
 from collections import defaultdict
 
+import yaml
 from ctf_parser import logger
 from ctf_parser.grammar.pcfg import PCFG
+from ctf_parser.parser.ctf_mapper import CtfMapper
 
 
 def transform(pcfg, mapping, level=2):
     fine_to_coarse = mapping.fine_to_coarse[level]
     symbol = pcfg.get_word_for_id
 
+    def replace_symbols(text):
+        unary_split = text.split("‡")
+        unaries_replaced = []
+        for unary in unary_split:
+            binarize_split = unary.split("†")
+            binaries_replaced = []
+            for binary in binarize_split:
+                binaries_replaced.append(fine_to_coarse.get(binary, binary))
+            unaries_replaced.append("†".join(binaries_replaced))
+        ret = "‡".join(unaries_replaced)
+        return ret
     """
     Replace Symbols:
     
@@ -18,14 +32,15 @@ def transform(pcfg, mapping, level=2):
     transformed_rules = defaultdict(list)
 
     for lhs, rules in enumerate(pcfg.id_to_lhs):
-        lhs_t = fine_to_coarse.get(symbol(lhs), symbol(lhs))
 
         for rule in rules:
             if len(rule) == 4:
-                _, rhs1, rhs2, prob = rule
+                lhs, rhs1, rhs2, prob = rule
 
-                rhs1_t = fine_to_coarse.get(symbol(rhs1), symbol(rhs1))
-                rhs2_t = fine_to_coarse.get(symbol(rhs2), symbol(rhs2))
+                lhs_t = replace_symbols(symbol(lhs))
+
+                rhs1_t = replace_symbols(symbol(rhs1))
+                rhs2_t = replace_symbols(symbol(rhs2))
                 prob_t = math.pow(math.e, prob)
 
                 transformed_rules[lhs_t].append((lhs_t, rhs1_t, rhs2_t, prob_t))
@@ -36,9 +51,10 @@ def transform(pcfg, mapping, level=2):
                     f"=> {lhs_t} -> {rhs1_t} {rhs2_t} [{prob_t}]")
 
             elif len(rule) == 3:
-                _, rhs1, prob = rule
+                lhs, rhs1, prob = rule
 
-                rhs1_t = symbol(rhs1)
+                lhs_t = replace_symbols(symbol(lhs))
+                rhs1_t = replace_symbols(symbol(rhs1))
                 prob_t = math.pow(math.e, prob)
 
                 transformed_rules[lhs_t].append((lhs_t, rhs1_t, prob_t))
@@ -92,9 +108,26 @@ def transform(pcfg, mapping, level=2):
     return final_rules
 
 
-def transform_to_new_grammar(pcfg, mapping, level=2):
+def transform_to_new_grammar(pcfg, mapping, level=2, save=False,
+                             prefix="grammar"):
     new_pcfg = PCFG()
     new_grammar = transform(pcfg, mapping, level)
-    print(new_grammar)
     new_pcfg.load_model(new_grammar)
+
+    if save:
+        with open(f"{prefix}_{level}.pcfg", "w") as f:
+            logger.info(f"Write to file (level {level})...")
+            for l in new_grammar:
+                f.write(json.dumps(l) + "\n")
+
     return new_pcfg
+
+
+if __name__ == '__main__':
+    pcfg = PCFG()
+    pcfg.load_model([json.loads(l) for l in open("grammar.pcfg")])
+    mapping = CtfMapper(yaml.load(open("data/ctf_mapping.yml")))
+
+    for i in range(2, -1, -1):
+        pcfg = transform_to_new_grammar(pcfg, mapping, i, save=True,
+                                        prefix="grammar")
